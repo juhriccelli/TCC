1e2f4432cdf374af05e4393fd7f3f3caaa6b873e
“juhriccelli” <juliano.riccellii@gmail.com>
2019-06-28 14:28:59 -0300

    Versão 1.3.1

--- banco/inserirEleicoes.php
+++ banco/inserirEleicoes.php
@@ -14,6 +14,7 @@
       $dados = explode(';', $linha);
 
       $tamD = count($dados);
+      $ano = str_replace("\"", "",$dados[2]);
 
       //Retirar aspas do arquivo CSV
       for($i = 0; $i <$tamD; $i++){
@@ -22,9 +23,10 @@
       }
 
       //Verifica o ano da eleição para melhor preenchimento dos dados.
-      if($dados[3] >= 2016){
+     if ($ano >= 2016) {
+       echo "teste 2018 <br />";
         //Verifica se a linha é o cabeçalho ou se está vazia. Caso não atenda essas condições, prepara as variáves para inserção no banco
-        if($dados[0] != 'DATA DA EXTRACAO' && !empty($linha)) {
+        /*if($dados[0] != 'DATA_GERACAO' && !empty($linha)) {
           $dt_geracao = date("Y-m-d",strtotime(str_replace('/','-',$dados[0])));
           $hh_geracao  = date("H:i:s", strtotime($dados[1]));
           $ano_eleicao = date("Y", strtotime($dados[2]));
@@ -83,14 +85,155 @@
 
             //Caso consiga inserir o sql, imprime a query na tela. Caso contrario aparece o erro.
             if ($conexao->query($sql) === TRUE) {
-              echo "Cadastro de " . $nome_filiado . " realizado com sucesso <br />";
+              echo "Cadastro de " . $nm_candidato . " realizado com sucesso <br />";
+            } else {
+              echo "Deu ruim: " . $conexao->error ."<br />";
+            }
+          }*/
+
+          elseif ($dados[2] == 2014) {
+        //Verifica se a linha é o cabeçalho ou se está vazia. Caso não atenda essas condições, prepara as variáves para inserção no banco
+        if($dados[0] != 'DATA_GERACAO' && !empty($linha)) {
+          $dt_geracao = date("Y-m-d",strtotime(str_replace('/','-',$dados[0])));
+          $hh_geracao  = date("H:i:s", strtotime($dados[1]));
+          $ano_eleicao = date("Y", strtotime($dados[2]));
+          $nr_turno = (integer) $dados[3];
+          $ds_eleicao = $dados[4];
+          $sg_uf = $dados[5];
+          $sg_ue = $dados[6];
+          $cd_municipio = (integer) $dados[7];
+          $nm_municipio = $dados[8];
+          $nr_zona = (integer) $dados[9];
+          $cd_cargo = (integer) $dados[10];
+          $nr_candidato = (integer) $dados[11];
+          $sq_candidato = (integer) $dados[12];
+          $nm_candidato = $dados[13];
+          $nm_urna_candidato = $dados[14];
+          $ds_cargo = $dados[15];
+          $cd_situacao_candidatura = (integer) $dados[18];
+          $ds_situacao_candidatura = $dados[19];
+          $cd_sit_tot_turno = (integer) $dados[20];
+          $ds_sit_tot_turno = $dados[21];
+          $nr_partido = (integer) $dados[22];
+          $sg_partido = $dados[23];
+          $nm_partido = $dados[24];
+          $sq_coligacao = (integer) $dados[25];
+          $nm_coligacao = $dados[26];
+          $ds_composicao_coligacao = $dados[27];
+          $qt_votos_nominais = (integer) $dados[28];
+          $st_voto_em_transito = $dados[29];
+
+          $cd_tipo_eleicao = NULL;
+          $nm_tipo_eleicao = NULL;
+          $cd_eleicao = NULL;
+          $dt_eleicao = NULL;
+          $tp_abrangencia = NULL;
+          $nm_ue = NULL];
+          $nm_social_candidato = NULL;
+          $cd_detalhe_situacao_cand = NULL;
+          $ds_detalhe_situacao_cand = NULL;
+          $tp_agremiacao = NULL;
+
+          //SQL com o Insert pra ser inserido no banco.
+          $sql = "INSERT INTO eleicoes (dt_geracao, hh_geracao, ano_eleicao, cd_tipo_eleicao, nm_tipo_eleicao,
+            nr_turno, cd_eleicao, ds_eleicao, dt_eleicao, tp_abrangencia, sg_uf, sg_ue, nm_ue, cd_municipio,
+            nm_municipio, nr_zona, cd_cargo, ds_cargo, sq_candidato, nr_candidato, data_filiacao, situacao_registro,
+            tipo_registro, data_processamento, data_desfiliacao, nm_candidato, nm_urna_candidato, nm_social_candidato,
+            cd_situacao_candidatura, ds_situacao_candidatura, cd_detalhe_situacao_cand, ds_detalhe_situacao_cand,
+            tp_agremiacao, nr_partido, sg_partido, nm_partido, sq_coligacao, nm_coligacao, ds_composicao_coligacao,
+            cd_sit_tot_turno, ds_sit_tot_turno, st_voto_em_transito, qt_votos_nominais)
+            VALUES ('$dt_geracao', '$hh_geracao', '$ano_eleicao', '$cd_tipo_eleicao', '$nm_tipo_eleicao', '$nr_turno',
+              '$cd_eleicao', '$ds_eleicao', '$dt_eleicao', '$tp_abrangencia', '$sg_uf, '$sg_ue', '$nm_ue', '$cd_municipio',
+              '$nm_municipio', '$nr_zona', '$cd_cargo', '$ds_cargo', '$sq_candidato', '$nr_candidato', '$data_filiacao',
+              '$situacao_registro', '$tipo_registro', '$data_processamento', '$data_desfiliacao', '$nm_candidato',
+              '$nm_urna_candidato', '$nm_social_candidato', '$cd_situacao_candidatura', '$ds_situacao_candidatura',
+              '$cd_detalhe_situacao_cand', '$ds_detalhe_situacao_cand', '$tp_agremiacao', '$nr_partido', '$sg_partido',
+              '$nm_partido', '$sq_coligacao', '$nm_coligacao', '$ds_composicao_coligacao', '$cd_sit_tot_turno',
+              '$ds_sit_tot_turno', '$st_voto_em_transito', '$qt_votos_nominais')";
+
+            //Caso consiga inserir o sql, imprime a query na tela. Caso contrario aparece o erro.
+            if ($conexao->query($sql) === TRUE) {
+              echo "Cadastro de " . $nm_candidato . " realizado com sucesso <br />";
             } else {
               echo "Deu ruim: " . $conexao->error ."<br />";
             }
           }
       }
 
+      else {
+        //Verifica se a linha é o cabeçalho ou se está vazia. Caso não atenda essas condições, prepara as variáves para inserção no banco
+        if($dados[0] != 'DATA_GERACAO' && !empty($linha)) {
+          $dt_geracao = date("Y-m-d",strtotime(str_replace('/','-',$dados[0])));
+          $hh_geracao  = date("H:i:s", strtotime($dados[1]));
+          $ano_eleicao = date("Y", strtotime($dados[2]));
+          $nr_turno = (integer) $dados[3];
+          $ds_eleicao = $dados[4];
+          $sg_uf = $dados[5];
+          $sg_ue = $dados[6];
+          $cd_municipio = (integer) $dados[7];
+          $nm_municipio = $dados[8];
+          $nr_zona = (integer) $dados[9];
+          $cd_cargo = (integer) $dados[10];
+          $nr_candidato = (integer) $dados[11];
+          $sq_candidato = (integer) $dados[12];
+          $nm_candidato = $dados[13];
+          $nm_urna_candidato = $dados[14];
+          $ds_cargo = $dados[15];
+          $cd_situacao_candidatura = (integer) $dados[18];
+          $ds_situacao_candidatura = $dados[19];
+          $cd_sit_tot_turno = (integer) $dados[20];
+          $ds_sit_tot_turno = $dados[21];
+          $nr_partido = (integer) $dados[22];
+          $sg_partido = $dados[23];
+          $nm_partido = $dados[24];
+          $sq_coligacao = (integer) $dados[25];
+          $nm_coligacao = $dados[26];
+          $ds_composicao_coligacao = $dados[27];
+          $qt_votos_nominais = (integer) $dados[28];
+
+          $st_voto_em_transito = NULL;
+          $cd_tipo_eleicao = NULL;
+          $nm_tipo_eleicao = NULL;
+          $cd_eleicao = NULL;
+          $dt_eleicao = NULL;
+          $tp_abrangencia = NULL;
+          $nm_ue = NULL];
+          $nm_social_candidato = NULL;
+          $cd_detalhe_situacao_cand = NULL;
+          $ds_detalhe_situacao_cand = NULL;
+          $tp_agremiacao = NULL;
+
+          //SQL com o Insert pra ser inserido no banco.
+          $sql = "INSERT INTO eleicoes (dt_geracao, hh_geracao, ano_eleicao, cd_tipo_eleicao, nm_tipo_eleicao,
+            nr_turno, cd_eleicao, ds_eleicao, dt_eleicao, tp_abrangencia, sg_uf, sg_ue, nm_ue, cd_municipio,
+            nm_municipio, nr_zona, cd_cargo, ds_cargo, sq_candidato, nr_candidato, data_filiacao, situacao_registro,
+            tipo_registro, data_processamento, data_desfiliacao, nm_candidato, nm_urna_candidato, nm_social_candidato,
+            cd_situacao_candidatura, ds_situacao_candidatura, cd_detalhe_situacao_cand, ds_detalhe_situacao_cand,
+            tp_agremiacao, nr_partido, sg_partido, nm_partido, sq_coligacao, nm_coligacao, ds_composicao_coligacao,
+            cd_sit_tot_turno, ds_sit_tot_turno, st_voto_em_transito, qt_votos_nominais)
+            VALUES ('$dt_geracao', '$hh_geracao', '$ano_eleicao', '$cd_tipo_eleicao', '$nm_tipo_eleicao', '$nr_turno',
+              '$cd_eleicao', '$ds_eleicao', '$dt_eleicao', '$tp_abrangencia', '$sg_uf, '$sg_ue', '$nm_ue', '$cd_municipio',
+              '$nm_municipio', '$nr_zona', '$cd_cargo', '$ds_cargo', '$sq_candidato', '$nr_candidato', '$data_filiacao',
+              '$situacao_registro', '$tipo_registro', '$data_processamento', '$data_desfiliacao', '$nm_candidato',
+              '$nm_urna_candidato', '$nm_social_candidato', '$cd_situacao_candidatura', '$ds_situacao_candidatura',
+              '$cd_detalhe_situacao_cand', '$ds_detalhe_situacao_cand', '$tp_agremiacao', '$nr_partido', '$sg_partido',
+              '$nm_partido', '$sq_coligacao', '$nm_coligacao', '$ds_composicao_coligacao', '$cd_sit_tot_turno',
+              '$ds_sit_tot_turno', '$st_voto_em_transito', '$qt_votos_nominais')";
+
+            //Caso consiga inserir o sql, imprime a query na tela. Caso contrario aparece o erro.
+            if ($conexao->query($sql) === TRUE) {
+              echo "Cadastro de " . $nm_candidato . " realizado com sucesso <br />";
+            } else {
+              echo "Deu ruim: " . $conexao->error ."<br />";
+            }
+          }
+      }
+
+
+        }
+
     }
     fclose($arquivo);
   };
+
 ?>
--- index.php
+++ index.php
@@ -1,55 +1,28 @@
 <?php
-
-function scan_Dir($dir) {
-
-$arrfiles = array();
-
-if (is_dir($dir)) {
-
-if ($handle = opendir($dir)) {
-
-chdir($dir);
-
-while (false !== ($file = readdir($handle))) {
-
-if ($file != "." && $file != "..") {
-
-if (is_dir($file)) {
-
-$arr = scan_Dir($file);
-
-foreach ($arr as $value) {
-
-$arrfiles[] = $dir."/".$value;
-
-}
-
-} else {
-
-$arrfiles[] = $dir."/";
-
-}
-
-}
-
-}
-
-chdir("../");
-
-}
-
-closedir($handle);
-
-}
-
-return $arrfiles;
-
-}
-
-echo "<pre>";
-
-print_r(scan_Dir("."));
-
-echo "</pre>";
-
+  function scan_Dir($dir) {
+    $arrfiles = array();
+    if (is_dir($dir)) {
+      if ($handle = opendir($dir)) {
+        chdir($dir);
+        while (false !== ($file = readdir($handle))) {
+          if ($file != "." && $file != "..") {
+            if (is_dir($file)) {
+              $arr = scan_Dir($file);
+              foreach ($arr as $value) {
+                $arrfiles[] = $dir."/".$value;
+              }
+            } else {
+              $arrfiles[] = $dir."/";
+            }
+          }
+        }
+        chdir("../");
+        echo "<pre>";
+        print_r(scan_Dir($arrfiles));
+        echo "</pre>";
+      }
+      closedir($handle);
+    }
+    return $arrfiles;
+  }
 ?>
