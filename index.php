99e90f130b1dbd7dc1e6c80713a465612c11dfeb
“juhriccelli” <juliano.riccellii@gmail.com>
2019-07-04 14:10:40 -0300

    Versão 1.3.6

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <html lang=”pt-br”>
 <head>
   <meta charset=”UTF-8”>
+  <link rel="stylesheet" href="assets/estilo.css">
   <link rel="stylesheet" href="assets/bootstrap.min.css">
   <script src="assets/jquery.min.js"></script>
   <script src="assets/bootstrap.min.js"></script>
--- pages/barra.php
+++ pages/barra.php
@@ -2,19 +2,16 @@
 <html lang="pt-br">
 
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta charset=”UTF-8”>
+  <link rel="stylesheet" href="../assets/estilo.css">
   <link rel="stylesheet" href="../assets/bootstrap.min.css">
   <script src="../assets/jquery.min.js"></script>
   <script src="../assets/bootstrap.min.js"></script>
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <span class="navbar-brand">CandMap</span>
 
     <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
       <ul class="navbar-nav mr-auto">
@@ -24,25 +21,10 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Ação</a>
-            <a class="dropdown-item" href="#">Outra ação</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Algo mais aqui</a>
-          </div>
-        </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#">Desativado</a>
+          <a class="nav-link" href="#">Desativado</a>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
-      </form>
     </div>
   </nav>
 </body>
