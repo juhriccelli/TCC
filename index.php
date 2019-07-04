3facf37a9706794b0f6656c470880170bf253fc0
“juhriccelli” <juliano.riccellii@gmail.com>
2019-07-04 17:04:21 -0300

    Versão 1.3.7

--- index.php
+++ index.php
@@ -12,12 +12,17 @@
   <meta charset=”UTF-8”>
   <link rel="stylesheet" href="assets/estilo.css">
   <link rel="stylesheet" href="assets/bootstrap.min.css">
+  <script src="assets/script.js"></script>
   <script src="assets/jquery.min.js"></script>
   <script src="assets/bootstrap.min.js"></script>
   <title>
     CandMap - Sistema de monitoramento eleitoral
   </title>
   <body>
-
+    <div class="capa">
+      <div>
+        <img id="brasil" class="svg brasil" src="assets/brasil.svg">
+      </div>
+    </div>
   </body>
   </html>
--- pages/barra.php
+++ pages/barra.php
@@ -5,6 +5,7 @@
   <meta charset=”UTF-8”>
   <link rel="stylesheet" href="../assets/estilo.css">
   <link rel="stylesheet" href="../assets/bootstrap.min.css">
+  <script src="../assets/script.js"></script>
   <script src="../assets/jquery.min.js"></script>
   <script src="../assets/bootstrap.min.js"></script>
 </head>
