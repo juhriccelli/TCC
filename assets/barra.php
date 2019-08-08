7d202335a011d629de7ed347bde3c897a73846a6
“juhriccelli” <juliano.riccellii@gmail.com>
2019-08-08 11:01:51 -0300

    Versão 1.4.2

--- assets/barra.php
+++ assets/barra.php
@@ -8,6 +8,7 @@
   <script src="../assets/js/script.js"></script>
   <script src="../assets/js/jquery.min.js"></script>
   <script src="../assets/js/bootstrap.min.js"></script>
+  <script src="../assets/js/ajax.js"></script>
 </head>
 
 <body>
@@ -16,14 +17,17 @@
 
     <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(página atual)</span></a>
+        <li class="nav-item">
+          <a class="nav-link" href="../index.php">Home <span class="sr-only">(página atual)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="../pages/buscarEstado.php">Busca por Estado</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Desativado</a>
+          <a class="nav-link" href="../pages/buscarPartido.php">Busca por Partido</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../pages/buscarNome.php">Busca por Nome</a>
         </li>
       </ul>
     </div>
---
+++ pages/resultadoEstado.php
@@ -0,0 +1,7 @@
+<script>
+function testeOnclick() {
+  echo 'Bem vindo ao SOpt';
+  $('.p40').css({'background': 'red'});
+}
+testeOnclick();
+</script>
